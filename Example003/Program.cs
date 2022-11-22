59fe356d55022796b6ea496eb7e34c16a53d354c
ArtemP <artem93.01@mail.ru>
2022-11-22 22:52:48 +0300

    random

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = new Random().Next(1,10); //1 2 3 4 5 6 ....
+﻿int numberA = new Random().Next(1,10); //1 2 3 4 5 6 .... 9 последжнее число на 1 меньше
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
